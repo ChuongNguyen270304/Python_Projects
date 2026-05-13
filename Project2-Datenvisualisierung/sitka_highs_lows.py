31dd4dd991a2a5b0086827774e64ef4f6b0e2eda
Pham Buu Chuong Nguyen <163377617+ChuongNguyen270304@users.noreply.github.com>
2026-05-13 14:44:08 +0700

    Add files via upload

---
+++ Project2-Datenvisualisierung/sitka_highs_lows.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path('weather_data/sitka_weather_2021_simple.csv')
+lines = path.read_text().splitlines()
+reader = csv.reader(lines)
+header_row = next(reader)
+
+#for index, column_header in enumerate(header_row):
+    #print(index, column_header)
+
+# Extrahiert die Datum, Hoechst- und Tiefsttemperaturen.
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
+    highs.append(high)
+    lows.append(low)
+
+# Stellt die Hoechsttemperaturen grafisch dar.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue',alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Formatiert das Diagramm.
+plt.title("Daily High and Low Temperatures - 2021", fontsize=24)
+plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
