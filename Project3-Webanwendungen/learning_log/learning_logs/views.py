30318ac3f6057c1531cd845dbec87334ee2987fd
Pham Buu Chuong Nguyen <163377617+ChuongNguyen270304@users.noreply.github.com>
2026-05-22 10:16:41 +0200

    Update views.py

--- Project3-Webanwendungen/learning_log/learning_logs/views.py
+++ Project3-Webanwendungen/learning_log/learning_logs/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
-from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
@@ -12,13 +14,14 @@ def index(request):
 @login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -32,7 +35,9 @@ def new_topic(request):
         # Daten übermittelt; Daten werden verarbeitet.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # Zeigt ein leeres oder ein als ungültig erkanntes Formular an.
     context = {'form' : form}
@@ -41,6 +46,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     if request.method != 'POST':
         # Keine Daten übermittelt; es wird ein leeres Formular erstellt.
         form = EntryForm()
@@ -60,6 +66,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Ursprüngliche Anforderung; das mit dem jetztigen Eintrag vorab 
@@ -72,3 +79,6 @@ def edit_entry(request, entry_id):
             return redirect('learning_logs:topic', topic_id=topic.id)
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
